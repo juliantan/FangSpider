2c2971e4b641bb40e4a6bab72d7ca1bb8c45b4a0
DESKTOP-0UPM60C\Glf <Glf9832@163.com>
2017-08-27 13:57:47 +0800

    数据存入mysql

--- FangSpider/FangSpider/pipelines.py
+++ FangSpider/FangSpider/pipelines.py
@@ -12,29 +12,36 @@ class FangspiderPipeline(object):
     def process_item(self, item, spider):
         
 
-        # conn = MySQLdb.connect(
-        #     host=settings.DB_Host,
-        #     user=settings.DB_User,
-        #     passwd=settings.DB_Pwd,
-        #     charset='utf8')
-        # cursor = conn.cursor()
-        # cursor.execute("""use FangSpider;""")
-        # cur = conn.cursor()
+        conn = MySQLdb.connect(
+            host=settings.DB_Host,
+            user=settings.DB_User,
+            passwd=settings.DB_Pwd,
+            charset='utf8')
+        cursor = conn.cursor()
+        cursor.execute("""use FangSpider;""")
+        cur = conn.cursor()
 
-        # cur.execute(
-        #     "insert into tb_newhouse(EstateArea,LouPanName,HuXing,Face,Region,Loc,Area,UnitPrice,TotalPrice,LoupanUrl,CrawlTime) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-        #     (item['EstateArea'].encode('utf8'),
-        #      item['Name'].encode('utf8'),
-        #      item['HuXing'].encode('utf8'),
-        #      item['Face'].encode('utf8'),
-        #      item['Region'].encode('utf8'),
-        #      item['Loc'].encode('utf8'),
-        #      item['Area'].encode('utf8'),
-        #      item['UnitPrice'].encode('utf8'),
-        #      item['TotalPrice'].encode('utf8'),
-        #      item['LoupanUrl'].encode('utf8'),
-        #      item['CrawlTime'].encode('utf8')))
-        # cur.close()
-        # conn.commit()
-        # conn.close()
+        cur.execute(
+            "insert into tb_fanghouse(EstateArea,LouPanName,UnitNum,RefPrice,AvePrice,Mortgage,HuXing,LouDong,Floor,BuildCate,Area,Street,Position,Face,Unit,LouPanUrl,CrawlTime) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            (item['EstateArea'].encode('utf8'),
+             item['LouPanName'].encode('utf8'),
+             item['UnitNum'].encode('utf8'),
+             item['RefPrice'].encode('utf8'),
+             item['AvePrice'].encode('utf8'),
+             item['Mortgage'].encode('utf8'),
+             item['HuXing'].encode('utf8'),
+             item['LouDong'].encode('utf8'),
+             item['Floor'].encode('utf8'),
+             item['BuildCate'].encode('utf8'),
+             item['Area'].encode('utf8'),
+             item['Street'].encode('utf8'),
+             item['Position'].encode('utf8'),
+             item['Face'].encode('utf8'),
+             item['Unit'].encode('utf8'),
+             item['LouPanUrl'].encode('utf8'),
+             item['CrawlTime'].encode('utf8')
+             ))
+        cur.close()
+        conn.commit()
+        conn.close()
         return item
--- FangSpider/FangSpider/spiders/NewFangSpider.py
+++ FangSpider/FangSpider/spiders/NewFangSpider.py
@@ -38,7 +38,7 @@ class NewfangspiderSpider(scrapy.Spider):
             print e
 
     def parseLoupanInfo(self,response):
-        try:
+        # try:
             # print 'response.url:' + response.url
             items = FangspiderItem()
             if re.findall(re.compile(u'(.*?)新房'),response.xpath('//div[@id="fyxq_B01_03"]/ul[@class="tf f12"]/li[2]/a/text()').extract()[0])[0]:
@@ -46,73 +46,73 @@ class NewfangspiderSpider(scrapy.Spider):
             else:
             	items['EstateArea'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/p[@id="fyxq_B01_05"]/a/text()').extract()[0]:
+            if response.xpath('//div[@class="right_box"]/p[@id="fyxq_B01_05"]/a/text()').extract():
             	items['LouPanName'] = response.xpath('//div[@class="right_box"]/p[@id="fyxq_B01_05"]/a/text()').extract()[0]
             else:
             	items['LouPanName'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/p[@id="fyxq_B01_05"]/strong/text()').extract()[0]:
+            if response.xpath('//div[@class="right_box"]/p[@id="fyxq_B01_05"]/strong/text()').extract():
             	items['UnitNum'] = response.xpath('//div[@class="right_box"]/p[@id="fyxq_B01_05"]/strong/text()').extract()[0]
             else:
             	items['UnitNum'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/div[@id="right_box_zj"]/p/span[2]').xpath('string(.)').extract()[0]:
+            if response.xpath('//div[@class="right_box"]/div[@id="right_box_zj"]/p/span[2]').xpath('string(.)').extract():
             	items['RefPrice'] = response.xpath('//div[@class="right_box"]/div[@id="right_box_zj"]/p/span[2]').xpath('string(.)').extract()[0]
             else:
             	items['RefPrice'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/div[@class="right_box_lpj"]/p/span[@class="cl_666"]/span/text()').extract()[0]:
+            if response.xpath('//div[@class="right_box"]/div[@class="right_box_lpj"]/p/span[@class="cl_666"]/span/text()').extract():
             	items['AvePrice'] = response.xpath('//div[@class="right_box"]/div[@class="right_box_lpj"]/p/span[@class="cl_666"]/span/text()').extract()[0]
             else:
             	items['AvePrice'] = '/'
 
             sub =response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14"]')
-            if sub[0].xpath('string(.)').extract()[0]:
+            if sub[0].xpath('string(.)').extract():
             	items['Mortgage'] = sub[0].xpath('string(.)').extract()[0]
             else:
             	items['Mortgage'] = '/'
 
-            if sub[1].xpath('string(.)').extract()[0]:
+            if sub[1].xpath('string(.)').extract():
             	items['HuXing'] = sub[1].xpath('string(.)').extract()[0]
             else:
             	items['HuXing'] = '/'
 
-            if sub[2].xpath('string(.)').extract()[0]:
+            if sub[2].xpath('string(.)').extract():
             	items['LouDong'] = sub[2].xpath('string(.)').extract()[0]
             else:
             	items['LouDong'] = '/'
 
-            if sub[3].xpath('string(.)').extract()[0]:
+            if sub[3].xpath('string(.)').extract():
             	items['Floor'] = sub[3].xpath('string(.)').extract()[0]
             else:
             	items['Floor'] = sub[3].xpath('string(.)').extract()[0]
 
-            if sub[4].xpath('string(.)').extract()[0]:
+            if sub[4].xpath('string(.)').extract():
             	items['BuildCate'] = sub[4].xpath('string(.)').extract()[0]
             else:
             	items['BuildCate'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/span[2]/a/text()').extract()[0]:
+            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/span[2]/a/text()').extract():
             	items['Area'] = response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/span[2]/a/text()').extract()[0]
             else:
             	items['Area'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/span[2]/a/text()').extract()[1]:
-            	items['Street'] = response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/span[2]/a/text()').extract()[1]
+            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/span[2]/a/text()').extract():
+            	items['Street'] = response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/span[2]/a/text()').extract()[0]
             else:
             	items['Street'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/a/text()').extract()[0]:
+            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/a/text()').extract():
             	items['Position'] = response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/ul[@class="right_box_zzlxnrl hidden"]/li[@class="f14 w700"]/a/text()').extract()[0]
             else:
             	items['Position'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/div[@class="right_box_zzlxnrr_more"]/ul[1]/li[1]/text()').extract()[0]:
+            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/div[@class="right_box_zzlxnrr_more"]/ul[1]/li[1]/text()').extract():
             	items['Face'] = response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/div[@class="right_box_zzlxnrr_more"]/ul[1]/li[1]/text()').extract()[0]
             else:
             	items['Face'] = '/'
 
-            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/div[@class="right_box_zzlxnrr_more"]/ul[1]/li[2]/text()').extract()[0]:
+            if response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/div[@class="right_box_zzlxnrr_more"]/ul[1]/li[2]/text()').extract():
             	items['Unit'] = response.xpath('//div[@class="right_box"]/div[3]/div[@class="right_box_zzlxnr"]/div[@class="right_box_zzlxnrr_more"]/ul[1]/li[2]/text()').extract()[0]
             else:
             	items['Unit'] = '/'
@@ -120,9 +120,9 @@ class NewfangspiderSpider(scrapy.Spider):
             items['LouPanUrl'] = response.url
             items['CrawlTime'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
             # print items['LouPanName']
-        except Exception as e:
-            print e
-        finally:
+        # except Exception as e:
+        #     print e
+        # finally:
             yield items 
 
 
